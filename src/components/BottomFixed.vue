37a789ee539f715773770ebe0a0f9da517f6addb
Jack Arana <arack.dev@gmail.com>
2023-12-18 19:42:36 -0500

    feat(components): add TopFixed and BottomFixed

---
+++ src/components/BottomFixed.vue
@@ -0,0 +1,59 @@
+<script setup>
+</script>
+
+<template>
+  <footer>
+    <div class="container">
+      <nav>
+        <a href=""><i class="pi pi-github"></i></a>
+        <a href=""><i class="pi pi-linkedin"></i></a>
+        <a href=""><i class="pi pi-instagram"></i></a>
+      </nav>
+      <div class="toggle-theme"><i class="pi pi-sun"></i></div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+  }
+
+  footer .container {
+    display: flex;
+    align-items: end;
+  }
+  nav {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    padding: 1rem;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: var(--color-text);
+    padding: 0.7rem;
+    & i {
+      font-size: 1.5rem;
+    }
+  }
+
+  nav a:hover {
+  }
+
+  nav a.router-link-exact-active {
+    color: var(--color-primary);
+  }
+  .toggle-theme {
+    padding: 1.5rem 0;
+    & i {
+      font-size: 2em;
+    }
+  }
+</style>
---
+++ src/components/TopFixed.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <a href="/" class="logo-label">Arack</a>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/my-projects">Projects</router-link>
+        <router-link to="/about-me">About</router-link>
+        <router-link to="/contact-me">Contact</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+  header .container {
+    display: flex;
+    align-items: start;
+  }
+
+  .logo-label {
+    font-size: 1.5rem;
+    font-weight: bold;
+    padding: 1rem 0;
+    color: white;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 1rem;
+    padding: 1rem 0;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: white;
+    transform: rotate(270deg);
+    padding: 0.7rem;
+    margin: 1rem 0;
+    transition: all .3s ease-in-out;
+  }
+
+  nav a:hover {
+    background-color: var(--color-background-soft);
+    transform: rotate(0deg);
+  }
+
+  nav a.router-link-exact-active {
+    background-color: var(--color-background-soft);
+    transform: rotate(0deg);
+  }
+</style>
