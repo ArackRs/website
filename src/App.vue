4e7e855c2308f2ecd57d5fdc92a7c56abda5c5b0
Jack Arana <arack.dev@gmail.com>
2023-12-18 19:45:28 -0500

    feat(app): add fixed components and footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TopFixed from "@/components/TopFixed.vue";
+import BottomFixed from "@/components/BottomFixed.vue";
 </script>
 
 <template>
+  <TopFixed />
   <RouterView />
+  <footer>
+    <p>&copy; 2023 - All rights reserved. Proudly created with Vue.js by Jack Arana</p>
+  </footer>
+  <BottomFixed />
 </template>
 
 <style scoped>
+  footer p {
+    text-align: center;
+    padding: 0.5rem 0;
+    background-color: var(--color-background-soft);
+  }
 </style>
