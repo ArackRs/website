1438f34e2a9343b74e3b84a95a01e5716846b028
Jack Arana <arack.dev@gmail.com>
2024-01-02 18:47:12 -0500

    chore: add primevue dependency

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
+import 'primevue/resources/primevue.min.css'
+import 'primevue/resources/themes/lara-dark-cyan/theme.css'
+import 'primeicons/primeicons.css';
 import './styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from "primevue/config";
+import Button from 'primevue/button';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue);
+
+app.component('pv-button', Button);
 
 app.mount('#app')
